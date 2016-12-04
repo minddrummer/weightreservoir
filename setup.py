0076d8409efdabc48eeca09619ae30decaf560da
Ke <kesang0156357@gmail.com>
2016-12-04 13:03:03 -0500

    add url link

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Ke Sang',
     author_email='kesang0156357@gmail.com',
     packages=['weightreservoir'],
-    url='',
+    url='https://github.com/minddrummer/weightreservoir',
     license='LICENSE.txt',
     description='reservoir sampling with or without weight from a stream of data',
     install_requires=[
